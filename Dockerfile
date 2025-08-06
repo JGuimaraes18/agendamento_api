15cfd69131fd0bf869d179a7119ed392fcd69687
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2025-08-06 16:22:27 -0300

    criar superusuario do django

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ COPY . .
 
 RUN python manage.py collectstatic --noinput
 
-CMD ["gunicorn", "agendamento_api.wsgi:application", "-b", "0.0.0.0:10000"]
+CMD ["sh", "-c", "python manage.py migrate && python create_superuser.py && gunicorn agendamento_api.wsgi:application --bind 0.0.0.0:10000"]
---
+++ create_superuser.py
@@ -0,0 +1,19 @@
+# create_superuser.py
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendamento_api.settings')
+django.setup()
+
+User = get_user_model()
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superusuário '{username}' criado com sucesso.")
+else:
+    print(f"Superusuário '{username}' já existe.")
