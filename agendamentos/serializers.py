03beaf873df3da7e8b70af430db81edcf7887a2a
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2025-09-05 15:25:30 -0300

    ajuste no serializer da consulta

--- agendamentos/serializers.py
+++ agendamentos/serializers.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
 from .models import Consulta
+from pacientes.models import Paciente
+from profissionais.models import Profissional, Especialidade, PlanoSaude
 
 class ConsultaSerializer(serializers.ModelSerializer):
     paciente_id = serializers.PrimaryKeyRelatedField(
         source="paciente",
-        queryset=Consulta._meta.get_field("paciente").remote_field.model.objects.all(),
+        queryset=Paciente.objects.all(),
         write_only=True,
         label="Paciente"
     )
+    profissional_id = serializers.PrimaryKeyRelatedField(
+        source="profissional",
+        queryset=Profissional.objects.all(),
+        write_only=True,
+        label="Profissional"
+    )
+    especialidade_id = serializers.PrimaryKeyRelatedField(
+        source="especialidade",
+        queryset=Especialidade.objects.all(),
+        write_only=True,
+        label="Especialidade"
+    )
+    plano_id = serializers.PrimaryKeyRelatedField(
+        source="plano",
+        queryset=PlanoSaude.objects.all(),
+        write_only=True,
+        required=False,
+        allow_null=True,
+        label="Plano de Saúde"
+    )
+
     paciente_nome = serializers.CharField(source="paciente.nome", read_only=True)
     profissional_nome = serializers.CharField(source="profissional.nome", read_only=True)
     especialidade_nome = serializers.CharField(source="especialidade.nome", read_only=True)
@@ -18,9 +41,9 @@ class ConsultaSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "paciente_id", "paciente_nome",
-            "profissional_nome",
-            "especialidade_nome",
-            "plano_nome",
+            "profissional_id", "profissional_nome",
+            "especialidade_id", "especialidade_nome",
+            "plano_id", "plano_nome",
             "tem_convenio",
             "data_hora",
             "realizada",
